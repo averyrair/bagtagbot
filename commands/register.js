9b37d9c4e8f82cc451d9abd2c139f4a07d012bd3
avery <averyrair@gmail.com>
2024-04-23 12:33:41 -0400

    register fixes

--- commands/register.js
+++ commands/register.js
@@ -10,24 +10,56 @@ module.exports = {
                 .setDescription('The name associated with your UDisc account (Full name not handle)')
                 .setRequired(true)),
 	async execute(interaction) {
-        await db.execute(
-            `call registerPlayer(
-                ${db.escape(interaction.user.id)},
-                ${db.escape(interaction.options.getString('name'))}
-            )`
-        );
 
-        let result = await new Promise((resolve, reject) => {
+        const playerName = interaction.options.getString('name');
+        
+        let discordIDResult = await new Promise((resolve, reject) => {
             db.query(
                 `call getPlayer(
                     ${db.escape(interaction.user.id)},
                     null
                 )`,
             (err, results) => {
-                return err ? reject(err) : resolve(results);
+                return err ? reject(err) : resolve(results[0][0]);
             });
         });
 
-        interaction.reply(`Registered with Tag #${result[0][0].tagNum}`);
+        if (discordIDResult) {
+            //discord account already associated with player
+            interaction.reply({
+                content: `Account already linked to player ${discordIDResult.playerName}.`,
+                ephemeral: true
+            });
+            return;
+        }
+
+        let playerNameResult = await new Promise((resolve, reject) => {
+            db.query(
+                `call getPlayer(
+                    null,
+                    ${db.escape(playerName)}
+                )`,
+            (err, results) => {
+                return err ? reject(err) : resolve(results[0][0]);
+            });
+        });
+        
+        if (playerNameResult) {
+            //player exists in database
+            db.query(
+                `call registerDiscord(${db.escape(interaction.user.id)}, 
+                ${db.escape(playerName)})`
+            );
+
+            interaction.reply(`Discord account successfully linked to player ${db.escape(playerName)}`)
+        }
+        else {
+            //player does not exist
+            interaction.reply({
+                content: `Player ${db.escape(playerName)} not found.` + 
+                    `If you were given a tag, ask a league admin to add you to the system`,
+                ephemeral: true
+            });
+        }
 	},
 };
